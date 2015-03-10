a0db5ce84b5f0e8cdb6cb3e3cd86f5782bc7e7f7
darknessdocetw <sky712933@gmail.com>
2015-03-10 11:27:18 +0800

    login-logout

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,11 +9,19 @@ class SessionController < ApplicationController
       log_in user
       redirect_to user
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+
+  end
+
   def destroy
+        log_out
+        redirect_to root_url
   end
 
 end
